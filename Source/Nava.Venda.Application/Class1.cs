7e3630efd9434820aebe674c74b0980108f77526
Leonardo <bertolini.leo@smartfichas.com.br>
2020-10-31 12:08:56 -0300

    Criação da estrutura e projetos da aplicação.

---
+++ Source/Nava.Venda.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nava.Venda.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/Nava.Venda.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nava.Venda.Domain
+{
+    public class Class1
+    {
+    }
+}
